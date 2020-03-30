a5f5f2c57349785464af21865dd016cc7e2943a6
MatheusVaz17 <matheusvaz765@gmail.com>
2020-03-30 01:37:25 -0300

    Atualizações

--- index.php
+++ index.php
@@ -14,12 +14,16 @@ include "bd/conexao.php";
 <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+<script src="owl.carousel.min.js"></script>
+<link rel="stylesheet" href="owl.carousel.min.css">
+<link rel="stylesheet" href="owl.theme.default.min.css">
+<link rel="stylesheet" type="text/css" href="estilo_botoes/estilo.css">
 </head>
 <body>
 <!-- Navbar -->
 <div class="navbar-fixed">
   <nav>
-    <div class="nav-wrapper blue darken-3">
+    <div class="nav-wrapper #29b6f6 light-blue lighten-1">
       <a href="#!" class="brand-logo">Health Farm</a>
       <ul class="right hide-on-med-and-down">
         <li><a href="sass.html"><i class="material-icons left">search</i>Procurar produtos</a></li>
@@ -59,38 +63,135 @@ include "bd/conexao.php";
 <!--Products-->
   <h4>Medicamentos:</h4>
   <div class="row">
+  <div class="owl-carousel owl-theme owl-loaded" id="carousel1">
+    <div class="owl-stage-outer">
+      <div class="owl-stage">
 <?php
 $sql = "SELECT * FROM produto WHERE tipo = 'Medicamento' AND disponibilidade ='Disponivel'";
         $resultado = mysqli_query($connect, $sql);
-        if(mysqli_num_rows($resultado) > 0):
-        while ($dados = mysqli_fetch_array($resultado)):
+        if(mysqli_num_rows($resultado) > 0){ 
+        while ($dados = mysqli_fetch_array($resultado)){
         ?>
-            <div class="col s12 m3">
+        <div class="owl-item">
+            <div class="col s12 m10">
           <div class="card small hoverable">
             <span class="card-title"><?php echo $dados['nome']; ?></span>
-            <p><img width="250" height="160" src="fotos/<?php echo $dados['foto'] ?>"></p>
-            <p><h6><?php echo "Preço: R$".$dados['valor']; ?></h6></p>
-            <p><a style="align-self: center" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>Comprar</a></p>
+            <p><img width="130" height="100" src="fotos/<?php echo $dados['foto'] ?>"></p>
+            <div class="card-action">
+              <p><h6><?php echo "Preço: R$".$dados['valor']; ?></h6></p>
+            <p><a style="align-self: flex-end" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>Comprar</a></p>
           </div>
           </div>
+          </div>
+        </div>
+        <?php
+        }
+        }else{
+        ?>
+        <div class="owl-item">
+            <p>Não há medicamentos registrados!</p>
+        </div>
+        <?php
+        }
+        ?>
+</div>
+</div>
+</div>
+</div>
+
+<?php
+if (mysqli_num_rows($resultado) > 5) {
+?>
+<div class="navegacao">
+<button class="btn waves-effect waves-light #29b6f6 light-blue lighten-1" id="anterior"> <i class="material-icons">chevron_left</i></button>
+<button class="btn waves-effect waves-light #29b6f6 light-blue lighten-1" id="proximo"><i class="material-icons">chevron_right</i></button>
+</div>
+<?php } ?>
+
+
+<br>
+<h4>Higiene: </h4>
+<div class="row">
+  <div class="owl-carousel owl-theme owl-loaded" id="carousel2">
+    <div class="owl-stage-outer">
+      <div class="owl-stage">
+<?php
+$sql1 = "SELECT * FROM produto WHERE tipo = 'Higiene' AND disponibilidade ='Disponivel'";
+        $resultado1 = mysqli_query($connect, $sql1);
+        if(mysqli_num_rows($resultado1) > 0){
+        while ($dados = mysqli_fetch_array($resultado1)){
+        ?>
+        <div class="owl-item">
+            <div class="col s12 m10">
+          <div class="card small hoverable">
+            <span class="card-title"><?php echo $dados['nome']; ?></span>
+            <p><img width="130" height="100" src="fotos/<?php echo $dados['foto'] ?>"></p>
+            <div class="card-action">
+              <p><h6><?php echo "Preço: R$".$dados['valor']; ?></h6></p>
+            <p><a style="align-self: flex-end" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>Comprar</a></p>
+          </div>
+          </div>
+          </div>
+        </div>
 
         <?php
-        endwhile;
+        }
+      }else{
         ?>
+        <div class="owl-item">
+            <p>Não há Produtos de Higiene cadastrados!</p>
+        </div>
+        <?php
+        }
+        ?>
+
+        </div>
+        </div>
         </div>
       </div>
-        <?php
-        else: ?>
-        <p>Não há medicamentos registrados!</p>
-        <?php
-      endif;
+<?php
+if (mysqli_num_rows($resultado1) > 5) {
 ?>
+<div class="navegacao1">
+<button class="btn waves-effect waves-light #29b6f6 light-blue lighten-1" id="ant"> <i class="material-icons">chevron_left</i></button>
+<button class="btn waves-effect waves-light #29b6f6 light-blue lighten-1" id="prox"><i class="material-icons">chevron_right</i></button>
+</div>
+<?php } ?>
 <!-- Jquery's and JS-->
 <script>
 $(document).ready(function(){
     $('.slider').slider({indicators: false});
-    $('.carousel').carousel();
-  });
+
+    var owl1 = $("#carousel1");
+    owl1.owlCarousel({
+		items: 5
+	});
+
+    $('#anterior').on('click', function(){
+      owl1.trigger('prev.owl.carousel');
+    })
+
+    $('#proximo').on('click', function(){
+      owl1.trigger('next.owl.carousel');
+    })
+	
+	var owl2 = $("#carousel2");
+
+    owl2.owlCarousel({
+		items: 5
+	});
+
+    $('#ant').on('click', function(){
+      owl2.trigger('prev.owl.carousel');
+    })
+
+    $('#prox').on('click', function(){
+      owl2.trigger('next.owl.carousel');
+    })
+
+
+
+});
 
      
 </script>
